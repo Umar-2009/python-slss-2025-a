01c3fd6b757887fcf89b2a82da2557d699806cef
Umar Hassan <umarshassan09@gmail.com>
2025-10-07 14:18:50 -0700

    Added methods notes

---
+++ notes-1-intro.py
@@ -0,0 +1,39 @@
+# notes - introduction
+# 16 september 2025
+# Umar Hassan
+
+# create our own chatbot
+
+# 1. greet the user with a pre determined statement
+greeting = "Hello! I am a chatbot"
+
+# 2. Print the statement
+print(greeting)
+
+# 3. Introduce the bot
+print("My name is MeGPT")
+print("I'm not like the other chatbots")
+print("I'm completely deterministic")
+
+# 4. wow the user with some maths
+print("I bet you dont know what 8*8 is.")
+print("I can do it")
+print(f"8*8 is actually {8 * 8}.")
+print("what is pi squared")
+print("I can do it")
+print(f"it is {3.141592653**2}.")
+# 4. get the users name
+user_name = input(" what is your name?")
+print(f"it's nice to meet you, {user_name}.")
+print("I have some questions for you.")
+print("what is 50*2 ")
+print("are you sure?")
+print(f"it's {50 * 2}.")
+print("next question")
+print("what is my favourite color")
+color = input
+print(f"It is {color}.")
+print("last question...")
+print("what is your favourite animal?")
+animal = input
+print(f"{animal}. oh thats cool.")
