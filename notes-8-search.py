3a0f4020dcd8a265f7b7cec3cf0d3f60f578e9f4
Umar Hassan <umarshassan09@gmail.com>
2025-12-09 08:43:12 -0800

    Spotify explicit songs filter

--- notes-8-search.py
+++ notes-8-search.py
@@ -16,6 +16,7 @@ def main():
     artist_col = 2
     yt_views_col = 11
     tiktok_views_col = 15
+    explicit_col = 8
 
     # open the file
     with open("data/spotify2024.csv") as f:
@@ -26,14 +27,25 @@ def main():
         r = csv.reader(f)
 
         gunna_songs = []
+        clean_songs = []
 
         # read each line of data
         for info in r:
             if artist == info[artist_col]:
                 gunna_songs.append(info)
 
+                if info[explicit_col].lower() == "false":
+                    clean_songs.append(info)
+
         # print how many songs are in the list
         print(f"Number of Gunna Songs: {len(gunna_songs)}")
+        print(f"\nNon-Explicit {artist} Songs: {len(clean_songs)}\n")
+
+        for song in clean_songs:
+            print(f"Track: {song[track_col]}")
+            print(f"YouTube Views: {song[yt_views_col]}")
+            print(f"TikTok Views: {song[tiktok_views_col]}")
+            print("-" * 40)
 
 
 if __name__ == "__main__":
